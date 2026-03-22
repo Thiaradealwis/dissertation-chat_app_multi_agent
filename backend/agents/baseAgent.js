61d14aaf1772a21d49cbdaf1f47753454bbb2216
Thiara de Alwis <mq23793@bristol.ac.uk>
2026-03-22 15:16:18 +0000

    model change

--- backend/agents/baseAgent.js
+++ backend/agents/baseAgent.js
@@ -14,7 +14,7 @@ export default class Agent {
 
         if (!this.lastResponseId) {
             response = await this.client.responses.create({
-                model: "gpt-5-mini",
+                model: "gpt-5.4-mini",
                 input: [
                     {
                         role: "system",
@@ -28,7 +28,7 @@ export default class Agent {
             })
         } else {
             response = await this.client.responses.create({
-                model: "gpt-5-mini",
+                model: "gpt-5.4-mini",
                 previous_response_id: this.lastResponseId,
                 input: newMessages
             })
